82267df9e01c7b75adea7a0dc301b8bb574a5f1a
Tomi Aarnio <tomi.aarnio@nokia.com>
2017-10-03 17:05:29 +0300

    pnm.py: Do not check for file existence before trying to open it
    Use the recommended "with open(filename)" syntax instead. Also switch
    from IOError to RuntimeError when a file exists but is not valid.

--- pnm.py
+++ pnm.py
@@ -19,32 +19,28 @@ VERBOSE = True
 
 def read(filename):
     """
-    Reads in a PGM/PP file by the given name and returns its contents in a new numpy
+    Reads in a PGM/PPM file by the given name and returns its contents in a new numpy
     ndarray with 8/16-bit elements. Also returns the maximum representable value of a
     pixel (typically 255, 1023, 4095, or 65535).
     """
-    if os.path.isfile(filename):
-        with open(filename, "rb") as f:
-            buf = f.read()
-            regexPnmHeader = b"(^(P[56])\s+(\d+)\s+(\d+)\s+(\d+)\s)"
-            match = re.search(regexPnmHeader, buf)
-            if match is not None:
-                header, typestr, width, height, maxval = match.groups()
-                width, height, maxval = int(width), int(height), int(maxval)
-                typestr = typestr.decode("utf-8")
-                numch = 3 if typestr == "P6" else 1
-                shape = (height, width, numch) if typestr == "P6" else (height, width)
-                if VERBOSE:
-                    print("Reading file %s "%(filename), end='')
-                    print("(w=%d, h=%d, c=%d, maxval=%d)"%(width, height, numch, maxval))
-                dtype = ">u2" if maxval > 255 else np.uint8
-                pixels = np.frombuffer(buf, dtype, count=width*height*numch, offset=len(header))
-                pixels = pixels.reshape(shape).astype(np.uint8 if maxval <= 255 else np.uint16)
-                return pixels, maxval
-            else:
-                raise IOError("File %s is not a valid PGM/PPM file."%(filename))
-    else:
-        raise IOError("File %s does not exist."%(filename))
+    with open(filename, "rb") as f:
+        buf = f.read()
+        regexPnmHeader = b"(^(P[56])\s+(\d+)\s+(\d+)\s+(\d+)\s)"
+        match = re.search(regexPnmHeader, buf)
+        if match is not None:
+            header, typestr, width, height, maxval = match.groups()
+            width, height, maxval = int(width), int(height), int(maxval)
+            numch = 3 if typestr == b"P6" else 1
+            shape = (height, width, numch) if typestr == b"P6" else (height, width)
+            if VERBOSE:
+                print("Reading file %s "%(filename), end='')
+                print("(w=%d, h=%d, c=%d, maxval=%d)"%(width, height, numch, maxval))
+            dtype = ">u2" if maxval > 255 else np.uint8
+            pixels = np.frombuffer(buf, dtype, count=width*height*numch, offset=len(header))
+            pixels = pixels.reshape(shape).astype(np.uint8 if maxval <= 255 else np.uint16)
+            return pixels, maxval
+        else:
+            raise RuntimeError("File %s is not a valid PGM/PPM file."%(filename))
 
 def write(filename, pixelArray, maxval=1023):
     """
