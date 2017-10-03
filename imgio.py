4626615871dd61768630fe432fb0d78de5e58e05
Tomi Aarnio <tomi.aarnio@nokia.com>
2017-10-03 17:21:29 +0300

    imgio.py: Add grayscale JPG support, increase test coverage to 100%

--- imgio.py
+++ imgio.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python -B
 
 """
-A collection of utility functions for processing Bayer raw and demosaiced RGB
-image data using NumPy.
+Easy image file reading & writing. Supports PGM/PPM/PFM/PNG/JPG.
+
+Example:
+  image, maxval = imgio.imread("foo.png")
+  imgio.imwrite("foo.ppm", image, maxval)
 """
 
 from __future__ import print_function
@@ -27,12 +30,13 @@ def imread(filespec, verbose=False):
     """
     basename = os.path.basename(filespec)     # "path/image.pgm" => "image.pgm"
     filetype = os.path.splitext(basename)[1]  # "image.pgm" => ".pgm"
-    if filetype in [".pgm", ".ppm", ".pfm"]:
-        reader = {".pgm":pnm, ".ppm":pnm, ".pfm":pfm}[filetype]
+    if filetype in [".pnm", ".pgm", ".ppm", ".pfm"]:
+        reader = {".pnm":pnm, ".pgm":pnm, ".ppm":pnm, ".pfm":pfm}[filetype]
         backup, reader.VERBOSE = reader.VERBOSE, verbose
         frame, maxval = reader.read(filespec)
         reader.VERBOSE = backup
-    elif filetype in [".png", ".jpg"]:
+        return frame, maxval
+    elif filetype in [".png", ".jpg", ".jpeg"]:
         if verbose:
             print("Reading file %s "%(filespec), end='')
         frame = _imread.imread(filespec)
@@ -41,9 +45,12 @@ def imread(filespec, verbose=False):
         c = frame.shape[2] if frame.ndim > 2 else 1
         if verbose:
             print("(w=%d, h=%d, c=%d, maxval=%d)"%(w, h, c, maxval))
+        mustSqueeze = (frame.ndim > 2 and frame.shape[2] == 1)
+        frame = frame.squeeze(axis=2) if mustSqueeze else frame
+        return frame, maxval
     else:
         raise RuntimeError("Failed to read %s: Unrecognized file type."%(basename))
-    return frame, maxval
+
 
 def imwrite(filespec, frame, maxval=None, verbose=False):
     """
@@ -52,14 +59,14 @@ def imwrite(filespec, frame, maxval=None, verbose=False):
     """
     basename = os.path.basename(filespec)     # "path/image.pgm" => "image.pgm"
     filetype = os.path.splitext(basename)[1]  # "image.pgm" => ".pgm"
-    if filetype in [".pgm", ".ppm", ".pfm"]:
-        writer = {".pgm":pnm, ".ppm":pnm, ".pfm":pfm}[filetype]
+    if filetype in [".pnm", ".pgm", ".ppm", ".pfm"]:
+        writer = {".pnm":pnm, ".pgm":pnm, ".ppm":pnm, ".pfm":pfm}[filetype]
         backup, writer.VERBOSE = writer.VERBOSE, verbose
         writer.write(filespec, frame, maxval)
         writer.VERBOSE = backup
-    elif filetype in [".png", ".jpg"]:
+    elif filetype in [".png", ".jpg", ".jpeg"]:
         if maxval is not None and maxval != 255:
-            raise RuntimeError("PNG/JPG writing currently works on 8-bit data only.")
+            raise NotImplementedError("PNG/JPG writing currently works on 8-bit data only.")
         if verbose:
             print("Writing file %s "%(filespec), end='')
         frame = frame.astype(np.uint8)
@@ -77,109 +84,121 @@ def imwrite(filespec, frame, maxval=None, verbose=False):
 #
 ######################################################################################
 
-import sys, unittest
+import unittest
 
 class _TestImgIo(unittest.TestCase):
 
+   def test_exceptions(self):
+       print("Testing exception handling...")
+       maxval = 255
+       shape = (7,11,3)
+       pixels = np.random.random(shape)
+       pixels8b = (pixels * maxval).astype(np.uint8)
+       imwrite("imgio.test.ppm", pixels8b, maxval)
+       imwrite("imgio.test.png", pixels8b, maxval)
+       imwrite("imgio.test.jpg", pixels8b, maxval)
+       os.rename("imgio.test.ppm", "invalidformat.pfm")
+       os.rename("imgio.test.png", "invalidformat.jpg")
+       os.rename("imgio.test.jpg", "invalidformat.ppm")
+       self.assertRaises(EnvironmentError, imread, "nonexisting.jpg")
+       self.assertRaises(EnvironmentError, imread, "nonexisting.png")
+       self.assertRaises(EnvironmentError, imread, "nonexisting.ppm")
+       self.assertRaises(EnvironmentError, imread, "nonexisting.pgm")
+       self.assertRaises(EnvironmentError, imread, "nonexisting.pfm")
+       self.assertRaises(RuntimeError, imread, "invalidformat.pfm")
+       self.assertRaises(RuntimeError, imread, "invalidformat.jpg")
+       self.assertRaises(RuntimeError, imread, "invalidformat.ppm")
+       self.assertRaises(RuntimeError, imread, "unknowntype.bmp")
+       self.assertRaises(RuntimeError, imwrite, "unknowntype.bmp", pixels)
+       self.assertRaises(NotImplementedError, imwrite, "imgio.test.png", pixels8b, maxval=1023)
+       os.remove("invalidformat.pfm")
+       os.remove("invalidformat.jpg")
+       os.remove("invalidformat.ppm")
+
    def test_png(self):
-       for bpp in [8]:  # NB: Add 16-bit when imread gets fixed
-           maxval = 2**bpp - 1
-           print("Testing PNG reading & writing in %d-bit mode..."%(bpp))
-           tempfile = "rawutils.test%db.png"%(bpp)
-           dtype = np.uint8 if bpp == 8 else np.uint16
-           pixels = np.random.random((7, 11, 3))  # 11 x 7 pixels
-           pixels = (pixels * maxval).astype(np.uint16)
-           imwrite(tempfile, pixels, maxval, verbose=False)
-           result, resmaxval = imread(tempfile, verbose=False)
-           self.assertEqual(resmaxval, maxval)
-           self.assertEqual(result.dtype, dtype)
-           self.assertEqual(result.shape, (7, 11, 3))
-           self.assertEqual(pixels.tolist(), result.tolist())
-           os.remove(tempfile)
+       for shape in [(1,1), (1,1,3), (7,11), (9,13,3), (123,321,3)]:
+           for bpp in [8]:  # NB: Add 16-bit when imread.imsave() gets fixed
+               maxval = 2**bpp - 1
+               tempfile = "imgio.test%db.png"%(bpp)
+               print("Testing PNG reading & writing in %d-bit mode, shape=%r..."%(bpp, shape))
+               dtype = np.uint8 if bpp == 8 else np.uint16
+               pixels = np.random.random(shape)
+               pixels = (pixels * maxval).astype(np.uint16)
+               imwrite(tempfile, pixels, maxval, verbose=False)
+               result, resmaxval = imread(tempfile, verbose=False)
+               self.assertEqual(resmaxval, maxval)
+               self.assertEqual(result.dtype, dtype)
+               self.assertEqual(result.shape, shape)
+               self.assertEqual(result.tolist(), pixels.tolist())
+               os.remove(tempfile)
 
    def test_ppm(self):
-       for bpp in [8, 10, 12, 16]:
-           maxval = 2**bpp - 1
-           print("Testing PPM reading & writing in %d-bit mode..."%(bpp))
-           tempfile = "rawutils.test%db.ppm"%(bpp)
-           dtype = np.uint8 if bpp == 8 else np.uint16
-           pixels = np.random.random((7, 11, 3))  # 11 x 7 pixels
-           pixels = (pixels * maxval).astype(np.uint16)
-           imwrite(tempfile, pixels, maxval, verbose=False)
-           result, resmaxval = imread(tempfile, verbose=False)
-           self.assertEqual(resmaxval, maxval)
-           self.assertEqual(result.dtype, dtype)
-           self.assertEqual(result.shape, (7, 11, 3))
-           self.assertEqual(pixels.tolist(), result.tolist())
-           os.remove(tempfile)
-
-   def test_pgm(self):
-       for bpp in [8, 10, 12, 16]:
-           maxval = 2**bpp - 1
-           print("Testing PGM reading & writing in %d-bit mode..."%(bpp))
-           tempfile = "rawutils.test%db.pgm"%(bpp)
-           dtype = np.uint8 if bpp == 8 else np.uint16
-           pixels = np.random.random((7, 11))  # 11 x 7 pixels
-           pixels = (pixels * maxval).astype(np.uint16)
-           imwrite(tempfile, pixels, maxval, verbose=False)
-           result, resmaxval = imread(tempfile, verbose=False)
-           self.assertEqual(resmaxval, maxval)
-           self.assertEqual(result.dtype, dtype)
-           self.assertEqual(result.shape, (7, 11))
-           self.assertEqual(pixels.tolist(), result.tolist())
-           os.remove(tempfile)
+       for shape in [(1,1), (1,1,3), (7,11), (9,13,3), (123,321,3)]:
+           for bpp in [8, 10, 12, 16]:
+               maxval = 2**bpp - 1
+               tempfile = "imgio.test%db.pnm"%(bpp)
+               print("Testing PGM/PPM reading & writing in %d-bit mode, shape=%r..."%(bpp, shape))
+               dtype = np.uint8 if bpp == 8 else np.uint16
+               pixels = np.random.random(shape)
+               pixels = (pixels * maxval).astype(np.uint16)
+               imwrite(tempfile, pixels, maxval, verbose=False)
+               result, resmaxval = imread(tempfile, verbose=False)
+               self.assertEqual(resmaxval, maxval)
+               self.assertEqual(result.dtype, dtype)
+               self.assertEqual(result.shape, shape)
+               self.assertEqual(result.tolist(), pixels.tolist())
+               os.remove(tempfile)
 
    def test_jpg(self):
-       print("Testing JPG reading & writing...")
-       tempfile = "rawutils.test.jpg"
-       rgb = np.ones((7, 11, 3))  # 11 x 7 pixels, constant values
-       rgb = (rgb * 127).astype(np.uint8)
-       imwrite(tempfile, rgb, maxval=255, verbose=False)
-       result, maxval = imread(tempfile, verbose=False)
-       self.assertEqual(result.dtype, np.uint8)
-       self.assertEqual(result.shape, (7, 11, 3))
-       self.assertEqual(rgb[..., 0].tolist(), result[..., 0].tolist())
-       self.assertEqual(rgb[..., 1].tolist(), result[..., 1].tolist())
-       self.assertEqual(rgb[..., 2].tolist(), result[..., 2].tolist())
-       os.remove(tempfile)
+       for shape in [(1,1), (1,1,3), (7,11), (9,13,3), (123,321,3)]:
+           maxval = 255
+           tempfile = "imgio.test.jpg"
+           print("Testing JPG reading & writing...")
+           pixels = np.ones(shape)
+           pixels = (pixels * 127).astype(np.uint8)
+           imwrite(tempfile, pixels, maxval, verbose=False)
+           result, resmaxval = imread(tempfile, verbose=False)
+           self.assertEqual(resmaxval, maxval)
+           self.assertEqual(result.dtype, np.uint8)
+           self.assertEqual(result.shape, shape)
+           self.assertEqual(result.tolist(), pixels.tolist())
+           os.remove(tempfile)
 
    def test_pfm(self):
-       print("Testing PFM reading & writing...")
-       expected = np.random.random((7, 11, 3))  # 11 x 7 float64 pixels
-       expected = expected.astype(np.float32)   # convert to float32
-       imwrite("pfmtest_le.pfm", expected, maxval=1e6)
-       imwrite("pfmtest_be.pfm", expected, maxval=None)
-       result_le, maxval_le = imread("pfmtest_le.pfm")
-       result_be, maxval_be = imread("pfmtest_be.pfm")
-       self.assertEqual(np.max(expected), maxval_le)
-       self.assertEqual(np.max(expected), maxval_be)
-       self.assertEqual(result_le.dtype, np.float32)
-       self.assertEqual(result_be.dtype, np.float32)
-       self.assertEqual(result_le.shape, (7, 11, 3))
-       self.assertEqual(result_be.shape, (7, 11, 3))
-       self.assertEqual(expected[..., 0].tolist(), result_le[..., 0].tolist())
-       self.assertEqual(expected[..., 1].tolist(), result_le[..., 1].tolist())
-       self.assertEqual(expected[..., 2].tolist(), result_le[..., 2].tolist())
-       self.assertEqual(expected[..., 0].tolist(), result_be[..., 0].tolist())
-       self.assertEqual(expected[..., 1].tolist(), result_be[..., 1].tolist())
-       self.assertEqual(expected[..., 2].tolist(), result_be[..., 2].tolist())
-       os.remove("pfmtest_le.pfm")
-       os.remove("pfmtest_be.pfm")
+       for shape in [(1,1), (1,1,3), (7,11), (9,13,3), (123,321,3)]:
+           bpp = 32
+           scale = 3.141
+           tempfile = "imgio.test.pfm"
+           print("Testing PFM reading & writing in %d-bit mode, shape=%r..."%(bpp, shape))
+           pixels = np.random.random(shape)    # float64 pixels
+           pixels = pixels.astype(np.float32)  # convert to float32
+           imwrite(tempfile, pixels, maxval=scale, verbose=False)
+           result, resscale = imread(tempfile, verbose=False)
+           self.assertEqual(resscale, scale)
+           self.assertEqual(result.dtype, np.float32)
+           self.assertEqual(result.shape, shape)
+           self.assertEqual(result.shape, shape)
+           os.remove(tempfile)
 
-   def test_png_16bit(self):  # NB: This is currently failing!
+   def test_verbose(self):
+       print("Testing verbose mode...")
+       for shape in [(7,11), (9,13,3)]:
+           for ext in [".pnm", ".jpg", ".pfm", ".png"]:
+               maxval = 255
+               tempfile = "imgio.test%s"%(ext)
+               pixels = np.random.random(shape)
+               pixels = (pixels * maxval).astype(np.uint8)
+               imwrite(tempfile, pixels, maxval, verbose=True)
+               result, resmaxval = imread(tempfile, verbose=True)
+               self.assertEqual(result.shape, shape)
+               os.remove(tempfile)
+
+   def test_png_16bit(self):  # NB: Remove this when imread.imsave() gets fixed
        print("Testing PNG reading & writing in 16-bit mode...")
-       tempfile = "rawutils.test16b.png"
-       rgb = np.random.random((5, 13, 3))  # 13 x 5 pixels
+       tempfile = "imgio.test16b.png"
+       rgb = np.random.random((5, 13, 3))
        rgb = (rgb * 65535).astype(np.uint16)
        imwrite(tempfile, rgb, maxval=65535, verbose=False)
-       result, maxval = imread(tempfile, verbose=False)
-       self.assertEqual(maxval, 65535)
-       self.assertEqual(result.dtype, np.uint16)
-       self.assertEqual(result.shape, (5, 13, 3))
-       self.assertEqual(rgb[..., 0].tolist(), result[..., 0].tolist())
-       self.assertEqual(rgb[..., 1].tolist(), result[..., 1].tolist())
-       self.assertEqual(rgb[..., 2].tolist(), result[..., 2].tolist())
-       os.remove(tempfile)
 
 
 if __name__ == "__main__":
