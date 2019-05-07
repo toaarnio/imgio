6cf875678e98abb9bbc6fe41db47d33bf97b1f90
Tomi Aarnio <tomi.p.aarnio@gmail.com>
2019-05-07 11:01:10 +0300

    Reduce the number of manual steps required to build, install & release
    Version number is now single-sourced from __init__.py, and dependencies
    from requirements.txt.
    
    This commit constitutes release 0.4.4.

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, find_packages
 
+import imgio
+
+def read_deps(filename):
+    with open(filename) as f:
+        deps = f.read().split('\n')
+        deps.remove("")
+    return deps
+
 setup(name='imgio',
-      version='0.4.3',
+      version=imgio.__version__,
       description='Easy image reading & writing. Supports PGM/PPM/PNM/PFM/PNG/TIFF/JPG/INSP/RAW.',
       url='http://github.com/toaarnio/imgio',
       author='Tomi Aarnio',
       author_email='tomi.p.aarnio@gmail.com',
       license='MIT',
       packages=find_packages(),
-      install_requires=['imread>=0.7.1', 'piexif>=1.1.2', 'numpy'],
+      install_requires=read_deps("requirements.txt"),
       test_suite='imgio',
       zip_safe=False)
